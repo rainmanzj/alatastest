caeef5518b48f12512488f6bfbd158506c85b1f8
张杰 <165590438@qq.com>
2017-08-12 18:37:43 +0800

    1

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -4,7 +4,28 @@ var async = require('async');
  * Module dependencies.
  */
 exports.index = function (req, res) {
-res.render('home/index', {
+  var elasticsearch = require('elasticsearch');
+  var client = new elasticsearch.Client({
+    host: 'localhost:9200',
+    log: 'trace'
+  });
+
+  client.search({
+    index: 'index',
+    type: 'type',
+    body: {
+      query: {
+        match: {
+          name: 'zj'
+        }
+      }
+    }
+  }).then(function (resp) {
+      var hits = resp.hits.hits;
+  }, function (err) {
+      console.trace(err.message);
+  });
+  res.render('home/index', {
     title: 'Node Express Mongoose Boilerplate'
   });
 };
