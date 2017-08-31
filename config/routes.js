437074e8d3f5da49ce5d7d6eda89c3bc1d5515dc
张杰 <165590438@qq.com>
2017-08-31 16:02:05 +0800

    1

--- config/routes.js
+++ config/routes.js
@@ -29,7 +29,7 @@ function getopenid(req, res, next) {
 
 module.exports = function (app) {
     //db.init();
-    app.get('/3d/testgetserver',home.testgetserver);
+    app.get('/3d',home.testgetserver);
     // app.get('/testgetserver', function (req, res) {
     //     home.testgetserver(req, res);
     // });
